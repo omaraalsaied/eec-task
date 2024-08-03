07c6d5b6265ef3f50de83002e1f2734132fb680a
omaraalsaied <omar2181998@yahoo.com>
2024-08-04 02:59:32 +0300

    console command to search cheapest price in all pharmacies

---
+++ app/Console/Commands/SearchCheapestPharmacies.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use App\Models\Pharmacy;
+use Illuminate\Console\Command;
+
+class SearchCheapestPharmacies extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:search-cheap {product_id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $product_id = $this->argument('product_id');
+
+        $product = Product::find($product_id);
+
+        if (!$product) {
+            $this->error('Invalid Product ID');
+            return 1;
+        }
+
+        $cheapestPharmacies = Pharmacy::join('pharmacy_product', 'pharmacies.id', '=', 'pharmacy_product.pharmacy_id')
+            ->where('pharmacy_product.product_id', $product_id)
+            ->orderBy('pharmacy_product.price', 'asc')
+            ->take(5)
+            ->get(['pharmacies.id', 'pharmacies.name', 'pharmacy_product.price']);
+
+        if ($cheapestPharmacies->isEmpty()) {
+            $this->info("No pharmacies found with this product.");
+            return 0;
+        }
+
+        $result = $cheapestPharmacies->map(function ($pharmacy) {
+            return [
+                'id' => $pharmacy->id,
+                'name' => $pharmacy->name,
+                'price' => $pharmacy->price,
+            ];
+        });
+
+        $this->line(json_encode($result, JSON_PRETTY_PRINT));
+
+        return 0;
+    }
+}
